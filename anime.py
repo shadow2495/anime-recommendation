7899f5c20965e3bf70120934fbe73767f75dc8cd
Harsh Raj <157479109+shadow2495@users.noreply.github.com>
2026-03-17 22:37:20 +0530

    Update anime.py

--- anime.py
+++ anime.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-
-import pickle
+#import pickle
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
